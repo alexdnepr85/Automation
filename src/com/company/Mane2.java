ef4118c79baeedaab67eb619801ea966b48a464f
Nikita <Ageevnikitatest@gmail.com>
2015-03-25 21:02:03 +0200

    First lesson with git

---
+++ src/com/company/Mane2.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * Created by Никита on 25/03/15.
+ */
+public class Mane2 {
+    public static void main(String[] args) {
+
+        /*String r = args[0];
+
+        if (r.equals("Hello")){
+            System.out.println("Hello");
+        }
+        else {
+            System.out.println("Good bye");}*/
+
+
+        String k = args[0];
+        String g = args[1];
+
+        if (k.equals(("Hello")) && g.equals("Class")){
+
+                                                         /* Делаем сравнивание два раза
+                                                          ( если К=Hello и G=Class тогда печатаем , если  не равно...)*/
+            System.out.println("Success");
+
+        }
+
+        else {
+            System.out.println("Good bye");
+        }
+
+        }
+
+    }
