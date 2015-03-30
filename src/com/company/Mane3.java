5a5faa7096f4755bfe184f6a0c5ca24fb5cf2908
Nikita <Ageevnikitatest@gmail.com>
2015-03-30 19:53:33 +0300

    First lesson with git

--- src/com/company/Mane3.java
+++ src/com/company/Mane3.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by Никита on 30/03/15.
  */
 public class Mane3 {
     public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Nikita/Work/Avtomatization/chromedriver.exe");
+        WebDriver p = new ChromeDriver();
 
 }}
