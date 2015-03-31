89caa96a0ef291c89028d75a3e9056c007a5ed17
Nikita <Ageevnikitatest@gmail.com>
2015-03-31 14:06:27 +0300

    First lesson with git

--- src/com/company/Door.java
+++ src/com/company/Door.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.thoughtworks.selenium.webdriven.commands.Open;
-
 /**
  * Created by Никита on 30/03/15.
  */
@@ -9,78 +7,39 @@ public class Door {
     public  int height;
     public  int width;
     public  boolean open;
-    public Door (int h,int w) {
+    public Door (int h,int w,boolean open) {
         this.height = h;
         this.width = w;
         this.open = false;
     }
 
     public static void main(String[] args) {
-        Door k = new Door(100,250);
-        Door g = new Door(200,350);
-        k.open = true;
-        if (k.height <100 ){
-            System.out.println("1");}
+
+        Door door1 = new Door(100,250,true);
+        Door door2 = new Door(200,350,false);
+        door1.open = true;
+        door2.open = false;
+
+        if (door1.height == 100 && door1.height < 260){
+
+            System.out.println("Все ок");}
+
         else {
             System.out.println("2");}
 
-        if (k.open != false) {
+        if (door1.open != false) {
             System.out.println("3");
 
         }
         else {
             System.out.println("4");}
-        }
+
+
+
+    while (door2.width<355) {
+        door2.width = door2.width + 1;
+        System.out.println("Цикл +1"+ " = " + door2.width);
     }
+    }}
 
 
-
-
-
-
-    /*public int height;
-    public int width;
-    public  boolean Open;
-
-    public Door(int h) {
-        height=h;
-        Open = true;
-    }
-
-    public static void main(String[] args) {
-
-        Door r = new Door(100);
-        Door k = new Door(200);
-        int d = r.height;
-        int p = k.width;
-        r.Open= true;
-    }
-
-
-
-    }
-
-      }
-
-
-
-
-
-
-/*{
-    public int height;
-    public int width;
-    public boolean isOpened;
-    public  Door(int h){
-        height = h;
-        isOpened = false;
-    }
-    Door k = new Door(100);
-int d = k.height;
-k.isOpened=true;
-
-        if (k.isOpened) {
-        System.out.println("Open");
-        }
-        else {
-        System.out.println("Close");*/
