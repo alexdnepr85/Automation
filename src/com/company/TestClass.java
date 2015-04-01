052768bfbb67f51f7440622018ac35003370c0a7
Nikita <Ageevnikitatest@gmail.com>
2015-04-01 16:51:29 +0300

    First lesson with git

--- src/com/company/TestClass.java
+++ src/com/company/TestClass.java
@@ -3,11 +3,29 @@ package com.company;
 /**
  * Created by Никита on 01/04/15.
  */
+
+
+
+
 public class TestClass {
-    public  int height = 2000;
-    public  int width = 1000;
+    public  int height;
+    public  int width ;
     public boolean opened;
     public boolean locked;
+
+
+
+
+    public  String toString(){
+
+    String s = "Size: " + height + "x" + width + "mm" + "\n"+
+               "Opened: " + "\n" +
+               "Locked: ";
+        return s;
+
+
+
+    }
 
     public  TestClass (int h,int w, boolean opened,boolean locked){
         this.height = h;
@@ -19,15 +37,11 @@ public class TestClass {
     }
 
     public static void main(String[] args) {
-        TestClass ProDoor1 = new TestClass(2000, 1000, true, false);
-        String DoorInfo1 = "Door1 Info :";
-
+        TestClass ProDoor1 = new TestClass(2000, 1000, false, false);
         System.out.println(DoorInfo1 + "\n" + ProDoor1);
 
-        TestClass ProDoor2 = new TestClass(2000, 1000, true, false);
+        TestClass ProDoor2 = new TestClass(3000, 2000, true, true);
         String DoorInfo2 = "Door2 Info :";
 
-        System.out.println("Door_2 info:\n" + ProDoor2);
+        System.out.println(DoorInfo2 + "\n" + ProDoor2);}}
 
-    }
-}
