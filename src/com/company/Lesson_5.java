4cb15f593f5a23617c1cd232a874a50d8e00aeb4
Nikita <Ageevnikitatest@gmail.com>
2015-04-07 11:27:19 +0300

    First lesson with git

--- src/com/company/Lesson_5.java
+++ src/com/company/Lesson_5.java
@@ -13,26 +13,47 @@ import java.util.List;
  */
 public class Lesson_5 {
 
+
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:/Nikita/Work/Avtomatization/chromedriver.exe");
         WebDriver p = new ChromeDriver();
         p.manage().window().maximize();
-        p.get (" http://angel.net/~nic/passwd.current.html");
+
+
+        p.get(" http://angel.net/~nic/passwd.current.html");
+
+        GetPass(p, "123456", "fsdfd@fre.fer");
+        GetPass(p, "sdgdfgdf", "fsdfd@gfyfguvhu.fer");
+        GetPass(p, "r76tr7t7", "fsdfdr6r56r56@fre.fer");
+
+
         WebElement s = p.findElement(By.name("master"));
         s.sendKeys("123456789");
         s = p.findElement(By.name("site"));
         s.sendKeys(Keys.ENTER);
         String K = p.findElement(By.name("password")).getAttribute("value");
         System.out.println(K);
+    }
+
+    public  static String GetPass (WebDriver j, String j1, String j2){
+        j.findElement(By.name("master")).sendKeys(j1);
+        j.findElement(By.name("site")).sendKeys(j2,Keys.ENTER);
+        String G = j.findElement(By.name("password")).getAttribute("value");
+        System.out.println(G);
+
+        return G ;
 
 
 
-        //p.quit();
+}}
 
 
 
 
 
 
-    }}
+
+
+
+
---
+++ src/com/company/Lesson_5_1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by Никита on 06/04/15.
+ */
+public class Lesson_5_1 {
+
+    public  static  void  main (String [] args){
+
+        System.out.println( summa (5,6));
+        System.out.println( summa (2,6));
+        System.out.println( summa (7,6));
+        System.out.println( summa (0,6));
+    }
+    public  static  int summa (int j, int j1) {
+        int res = j+j1;
+        return  res;
+    }
+}
