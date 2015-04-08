fd4144798ac7e641533d5cc8c840a51bea7ebab4
Nikita <Ageevnikitatest@gmail.com>
2015-04-08 20:17:38 +0300

    First lesson with git

--- src/com/company/Lesson_6_J_Unit.java
+++ src/com/company/Lesson_6_J_Unit.java
@@ -4,6 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 /**
@@ -26,7 +30,26 @@ public class Lesson_6_J_Unit {
         int a =10 ;
         int b = 6;
         int expectedResult =15;
-        Assert.assertEquals(expectedResult, a + b);  // класс Assert , в котором есть функции .
+        Assert.assertEquals(" Тащи  ! " ,expectedResult, a + b);
+                                                                    // класс Assert , в котором есть функции .
+    }
+
+    @Test
+
+    public  void  Test3(){
+        System.setProperty("webdriver.chrome.driver", "C:/Nikita/Work/Avtomatization/chromedriver.exe");
+        WebDriver p = new ChromeDriver();
+        p.manage().window().maximize();
+        p.get(" http://angel.net/~nic/passwd.current.html");
+        String j1 = "123456";
+        String j2 = "gmail.com";
+        p.findElement(By.name("master")).sendKeys(j1);
+        p.findElement(By.name("site")).sendKeys(j2, Keys.ENTER);
+        String G = p.findElement(By.name("password")).getAttribute("value");
+        String expectedresult2 = "DhpO4o+PKM3qA@1a";
+        Assert.assertEquals("Все работает " ,expectedresult2,G );
+        p.quit();
+
     }
 
 }
