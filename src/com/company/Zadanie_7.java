a5a8defe21b78e79531dd177e6494e289806ac8e
Nikita <Ageevnikitatest@gmail.com>
2015-04-14 19:39:27 +0300

    First lesson with git

--- src/com/company/Zadanie_7.java
+++ src/com/company/Zadanie_7.java
@@ -3,6 +3,7 @@ package com.company;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -12,29 +13,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+
 /**
  * Created by Никита on 14/04/15.
  */
+
 @RunWith(JUnit4.class)
 
 public class Zadanie_7 {
 
-
-
-    @Before
+    WebDriver dr;
 
 
 
-    public  void  Chrome(){
-        WebDriver brow;
+    @After
+
+    public void clean () {
+        dr.quit();}
+
+@Before
+    public  void  pered(){
         System.setProperty("webdriver.chrome.driver", "C:/Nikita/Work/Avtomatization/chromedriver.exe");
-        ChromeDriver brow = new ChromeDriver();
-        brow.manage().window().maximize();
-        brow.get(" http://angel.net/~nic/passwd.current.html");
-        WebElement masterField = dr.findElement(By.name("master"));
-        WebElement siteField = dr.findElement(By.name("site"));
+        dr = new ChromeDriver();
+        dr.manage().window().maximize();
+        dr.get(" http://angel.net/~nic/passwd.current.html");
 
-    }
+}
 
 
+@Test
+        public  void Test1 () {
+
+            dr.findElement(By.name("master")).sendKeys("12345678");
+            dr.findElement(By.name("site")).sendKeys("gmail.com", Keys.ENTER);
+            String password = dr.findElement(By.name("password")).getAttribute("value");
+            System.out.println("Пароль: " + password);
+  }
 }
