724d1ff3ab9dd14264a9c2be3e5c0fe05a7043a0
Nikita <Ageevnikitatest@gmail.com>
2015-07-07 10:25:20 +0300

    First lesson with git

--- src/com/company/PdfFiller/LessonsAll.java
+++ src/com/company/PdfFiller/LessonsAll.java
@@ -1,5 +1,7 @@
 package com.company.PdfFiller;
 
+import java.util.Scanner;
+
 /**
  * Created by Никита on 07/07/15.
  */
@@ -7,7 +9,10 @@ public class LessonsAll {
 
     public static void main (String[] args) {
 
-        System.out.println("Hello World");
+        System.out.print("Введите число : ");
+        Scanner sr = new Scanner(System.in); // ввод из консоли
+        int count = sr.nextInt(); // считать int
+        System.out.println(count*2);
 
 
 
