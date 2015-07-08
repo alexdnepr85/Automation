c80af04cc4d62dec30af2ec3a17cb2c6e83a272a
Nikita <Ageevnikitatest@gmail.com>
2015-07-08 18:38:50 +0300

    First lesson with git

--- src/com/company/lessonsPDFFiller/LessonsAll.java
+++ src/com/company/lessonsPDFFiller/LessonsAll.java
@@ -10,35 +10,26 @@ public class LessonsAll {
     public static void main (String[] args) {
 
         Scanner sr = new Scanner(System.in); // ввод из консоли
+
         System.out.println("Метод 1:  int -> char ");
         System.out.println("Метод 2:  float -> char ");
         System.out.println("Метод 3:  float -> char -> int ");
         System.out.print("Введите номер метода : ");
+
         int count = sr.nextInt(); // считать int
+
+
         if (count == 1 ) {
-            System.out.print("Введите число int : ");
-            int count1 = sr.nextInt();
-            char count2 = (char) (count1);
-            System.out.println("Char = " + count2);
+            P_O_Lessons.metodOne();
         }
 
         if (count == 2) {
-            System.out.print("Введите число float : ");
-            float count1 = sr.nextInt();
-            char count2 = (char) (count1);
-            System.out.println("Char = " + count2);
+            P_O_Lessons.metodTwo();
         }
 
         else {
-            System.out.print("Введите число float : ");
-            float count1 = sr.nextInt();
-            char count2 = (char) (count1);
-            int count3 = (int) count2;
-            System.out.println("Integer = " + count3);
+            P_O_Lessons.metodThree();
         }
-
-
-
 
 
     }
---
+++ src/com/company/lessonsPDFFiller/P_O_Lessons.java
@@ -0,0 +1,37 @@
+package com.company.lessonsPDFFiller;
+
+import java.util.Scanner;
+
+/**
+ * Created by Никита on 07/08/15.
+ */
+public class P_O_Lessons {
+
+    public static void metodOne () {
+        System.out.println("Выбран метод: int -> char");
+        Scanner sr = new Scanner(System.in);
+        System.out.print("Введите число int : ");
+        int count1 = sr.nextInt();
+        char count2 = (char) (count1);
+        System.out.print("Char = " + count2);
+
+    }
+    public static void metodTwo () {
+        System.out.println("Выбран метод: float -> char");
+        Scanner sr = new Scanner(System.in);
+        System.out.print("Введите число float : ");
+        float count1 = sr.nextFloat();
+        char count2 = (char) (count1);
+        System.out.println("Char = " + count2);
+
+    }
+    public static void metodThree () {
+        System.out.println("Выбран метод:  float -> char -> int");
+        Scanner sr = new Scanner(System.in);
+        System.out.print("Введите число float : ");
+        float count1 = sr.nextFloat();
+        char count2 = (char) (count1);
+        int count3 = (int) count2;
+        System.out.println("Integer = " + count3);
+}
+}
